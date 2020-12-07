e5612271914bbfbf66beb48ebc7101334902824a
h-wang <75626671+wxes51023@users.noreply.github.com>
2020-12-08 01:21:32 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int val;
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    val = i * j;
+                    System.Console.Write(i + "X" + j + "=");
+                    System.Console.Write("{0,3}", val.ToString() + " ");
+                }
+                System.Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
